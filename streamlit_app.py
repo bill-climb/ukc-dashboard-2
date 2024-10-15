7ac744845fdd1e65ff0b6699d863fe057db34421
bill-climb <billy.forsyth97@gmail.com>
2024-10-15 15:48:45 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,26 @@
 import pandas as pd
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+df1 = None 
+col1, col2, col3 = st.columns(3)
 
-uploaded_file = None
-while uploaded_file is None:
-    uploaded_file = st.file_uploader('upload ukc logbook file')
-df1=pd.read_excel(uploaded_file)
+with col1:
+    st.title("🎈 UKC Dashboard")
+    st.write(
+        "Welcome to the UKC Dashboard. Upload your data on the right hand side."
+    )
+
+with col2:
+    uploaded_file = None
+    
+    if uploaded_file is not None:
+        df1=pd.read_excel(uploaded_file)
+        st.table(df1)
+    else:
+        st.warning('you need to upload ukc logbook excel file.')
+        uploaded_file = st.file_uploader('upload ukc logbook file')
+
 st.table(df1)
 
 
-
-#uploaded_file = None
-
-#if uploaded_file is not None:
-#    df1=pd.read_excel(uploaded_file)
-#    st.table(df1)
-#else:
-#    st.warning('you need to upload ukc logbook excel file.')
-#    uploaded_file = st.file_uploader('upload ukc logbook file')
 
