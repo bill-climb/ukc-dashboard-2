7fe2ad57dfd7b7aff057604382b2c318a5f8906d
bill-climb <billy.forsyth97@gmail.com>
2024-10-15 15:42:54 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,11 +5,12 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+uploaded_file = None
 
-
-uploaded_file = st.file_uploader('upload ukc logbook file')
 if uploaded_file is not None:
     df1=pd.read_excel(uploaded_file)
     st.table(df1)
 else:
     st.warning('you need to upload ukc logbook excel file.')
+    uploaded_file = st.file_uploader('upload ukc logbook file')
+
