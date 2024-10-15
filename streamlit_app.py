f3cdcbc1a0ee18e80ce7fa33f99dbd349f565297
bill-climb <billy.forsyth97@gmail.com>
2024-10-15 15:38:27 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
+import pandas as pd
 import streamlit as st
 
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+
+uploaded_file = st.file_uploader(“Choose a file”)
+if uploaded_file is not None:
+   df1=pd.read_excel(uploaded_file)
+else:
+st.warning(“you need to upload ukc logbook excel file.”)
