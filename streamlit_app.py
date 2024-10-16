1c634112daa1d53617ff67191604cd6e63bc0a3b
bill-climb <billy.forsyth97@gmail.com>
2024-10-16 12:51:36 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -237,11 +237,17 @@ else:
     
     
     #content
-   
-    col1, col2, col3 = st.columns([1,3,3])
-    
+    col1, col2 = st.columns(2)
     with col1:
         st.metric(label="Total logs", value=total_counts_2024)
+
+    with col2:
+        #days of climbing
+        total_days=df['Date'].nunique()
+        st.metric(label="Days climbing",value=total_days
+                  
+    col2, col3 = st.columns([1,1])
+    
   
     with col2:
         st.markdown("<h1 style='text-align: center;'>2024 Profile</h1>", unsafe_allow_html=True)
